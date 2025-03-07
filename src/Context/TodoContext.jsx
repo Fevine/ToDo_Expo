a43b82ed46e5f070c4800ef94d8cb1d9c020887c
Albinoni <albinoni1423@gmail.com>
2025-03-07 12:07:42 +0400

    Todo context added

---
+++ src/Context/TodoContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+import useAsyncStorage from "../Hooks/useAsyncstorage";
+
+export const todosContext = createContext();
+
+export const TodoProvider = ({ children }) => {
+
+  const [Todos, setTodos] = useAsyncStorage("Todo", []);
+
+  const data = {
+    Todos,
+    setTodos,
+  };
+
+  return (
+    <todosContext.Provider value={data}>
+      {children}
+    </todosContext.Provider>
+  );
+};
+
+export const useTodo = () => useContext(todosContext);
